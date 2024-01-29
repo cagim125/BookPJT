d10a8ffbe37137878ce635dec38e0b382036d10d
cagim125 <cagim30@gmail.com>
2024-01-30 02:00:06 +0900

    BookPJT First Commit

---
+++ BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberService.java
@@ -0,0 +1,44 @@
+package com.office.library.admin.member;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+
+@Service
+public class AdminMemberService {
+	
+	final static public int ADMIN_ACCOUNT_ALREADY_EXIST = 0;
+	final static public int ADMIN_ACCOUNT_CREATE_SUCCESS = 1;
+	final static public int ADMIN_ACCOUNT_CREATE_FAIL = -1;
+	
+	@Autowired
+	AdminMemberDao adminMemberDao;
+	
+	public int createAccountConfirm(AdminMemberVo adminMemberVo) {
+		System.out.println("[AdminMemberService] createAccountConfirm()");
+		
+		boolean isMember = adminMemberDao.isAdminMember(adminMemberVo.getA_m_id());
+		
+		if (!isMember) {
+			int result = adminMemberDao.insertAdminAccount(adminMemberVo);
+			
+			if (result > 0 )
+				return ADMIN_ACCOUNT_CREATE_SUCCESS;
+			else 
+				return ADMIN_ACCOUNT_CREATE_FAIL;
+			
+			
+		} else {
+			return ADMIN_ACCOUNT_ALREADY_EXIST;
+		}
+		
+		
+	}
+	
+
+	
+}
