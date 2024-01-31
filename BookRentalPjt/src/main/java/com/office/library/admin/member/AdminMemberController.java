3f7d052278159f274040d07cef9bc75934d5e728
cagim125 <cagim30@gmail.com>
2024-01-31 23:11:23 +0900

    회원 정보 수정, 임시 패스워드 발급

--- BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberController.java
+++ BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/admin/member")
@@ -87,15 +89,97 @@ public class AdminMemberController {
 	
 	// 관리자 목록 페이지
 	@RequestMapping(value="/listupAdmin", method = RequestMethod.GET)
-	public String listupAdmin(Model model) {
+	public ModelAndView listupAdmin() {
 		System.out.println("[AdminMemberController] modifyAccountConfirm()");
 		
 		String nextPage = "admin/member/listup_admins";
 		
 		List<AdminMemberVo> adminMemberVos = adminMemberService.listupAdmin();
 		
-		model.addAttribute("adminMemberVos", adminMemberVos);
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName(nextPage);
+		modelAndView.addObject("adminMemberVos", adminMemberVos);
 		
+		return modelAndView;
+	}
+	
+	// 관리자 승인
+	@RequestMapping(value = "/setAdminApproval", method = RequestMethod.GET)
+	public String setAdminApproval(@RequestParam("a_m_no") int a_m_no) {
+		System.out.println("[AdminMemberController] setAdminApproval()");
+		
+		String nextPage = "redirect:/admin/member/listupAdmin";
+		
+		adminMemberService.setAdminApproval(a_m_no);
+		
+		return nextPage;
+	}
+	
+	// 회원 정보 수정 페이지
+	@GetMapping("/modifyAccountForm")
+	public String modifyAccountForm(HttpSession session) {
+		System.out.println("[AdminMemberController] modifyAccountForm()");
+		
+		String nextPage = "admin/member/modify_account_form";
+		
+		AdminMemberVo loginedAdminMemberVo = 
+				(AdminMemberVo) session.getAttribute("loginedAdminMemberVo");
+		
+		if (loginedAdminMemberVo == null)
+			nextPage = "redirect:/admin/member/loginForm";
+		
+		return nextPage;
+	}
+	
+	// 회원 정보 수정 확인
+	// @RequestMapping(value = "/modifyAccountConfirm", method = RequestMethod.POST
+	@RequestMapping("/modifyAccountConfirm")
+	public String modifyAccountConfirm(AdminMemberVo adminMemberVo, HttpSession session) {
+		System.out.println("[AdminMemberController] modifyAccountConfirm");
+		
+		String nextPage = "admin/member/modify_account_ok";
+		
+		int result = adminMemberService.modifyAccountConfirm(adminMemberVo);
+		
+		if ( result > 0 ) {
+			AdminMemberVo loginedAdminMemberVo =
+					adminMemberService.getLoginedAdminMemberVo(adminMemberVo.getA_m_no());
+			
+			session.setAttribute("loginedAdminMemberVo", loginedAdminMemberVo);
+			session.setMaxInactiveInterval(60 * 30);	
+		}else {
+			nextPage = "admin/member/modify_account_ng";
+		}
+		
+		return nextPage;
+	}
+	
+	// 비밀번호 찾기
+	@GetMapping("/findPasswordForm")
+	public String findPasswordForm() {
+		System.out.println("[AdminMemberService] findPasswordForm()");
+		
+		String nextPage = "admin/member/find_password_form";
+		
+		return nextPage;
+	}
+	//비밀번호 찾기 확인
+	@PostMapping("/findPasswordConfirm")
+	public String findPasswordConfirm(AdminMemberVo adminMemberVo) {
+		System.out.println("[AdminMemberController] findPasswordConfirm");
+		
+		String nextPage ="admin/member/find_password_ok";
+		
+//		System.out.println("a_m_id :" + adminMemberVo.getA_m_id());
+//		System.out.println("a_m_name :" + adminMemberVo.getA_m_name());
+//		System.out.println("a_m_mail :" + adminMemberVo.getA_m_mail());
+		
+		int result = adminMemberService.findPasswordConfirm(adminMemberVo);
+		
+		if (result <= 0) {
+			nextPage = "admin/member/find_password_ng";
+		}
+				
 		return nextPage;
 	}
 
--- BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberService.java
+++ BookRentalPjt/src/main/java/com/office/library/admin/member/AdminMemberService.java
@@ -1,8 +1,15 @@
 package com.office.library.admin.member;
 
+import java.security.SecureRandom;
+import java.util.Date;
 import java.util.List;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +26,15 @@ public class AdminMemberService {
 	
 	@Autowired
 	AdminMemberDao adminMemberDao;
+	
+	@Autowired
+	JavaMailSenderImpl javaMailSenderImpl;
+	
+	public void setAdminApproval(int a_m_no) {
+		System.out.println("[AdminMemberService] setAdminApproval()");
+		
+		int result = adminMemberDao.updateAdminAccount(a_m_no);
+	}
 	
 	public List<AdminMemberVo> listupAdmin() {
 		System.out.println("[AdminMemberService] listupAdmin()");
@@ -58,6 +74,97 @@ public class AdminMemberService {
 		return loginedAdminMemberVo;
 	}
 	
+	public int modifyAccountConfirm(AdminMemberVo adminMemberVo) {
+		System.out.println("[AdminMemberSerice] modifyAccountConfirm()");
+		
+		return adminMemberDao.updateAdminAccount(adminMemberVo);
+	}
+	
+	public AdminMemberVo getLoginedAdminMemberVo(int a_m_no) {
+		System.out.println("[AdminMemberSerice] getLoginedAdminMemberVo()");
+		
+		return adminMemberDao.selectAdmin(a_m_no);
+	}
+	
+	public int findPasswordConfirm(AdminMemberVo adminMemberVo) {
+		System.out.println("[AdminMemberService] findPasswordConfirm()");
+		
+		AdminMemberVo selectAdminMemberVo = 
+				adminMemberDao.selectAdmin(
+						adminMemberVo.getA_m_id(),
+						adminMemberVo.getA_m_name(),
+						adminMemberVo.getA_m_mail() 
+						);
+	
+//		System.out.println("selectAdminMemberVo : " + selectAdminMemberVo.a_m_id);
+	
+		int result = 0;
+		
+		if (selectAdminMemberVo != null) {
+//			System.out.println("selectAdminMemberVo가 null이면 실행이 안되는 구문");
+			String newPassword = createNewPassword();
+			result = adminMemberDao.updatePassword(adminMemberVo.getA_m_id(),newPassword);
+			
+			if (result > 0)
+				sendNewPasswordByMail(adminMemberVo.getA_m_mail(), newPassword);
+		} 
+		
+		System.out.println("result : " + result);
+		return result;
+	}
+	
+	private String createNewPassword() {
+		System.out.println("[AdminMemberService] createNewPassword()" );
+		
+		char[] chars = new char[] {
+				'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 
+				'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+				'u', 'v', 'w', 'x', 'y', 'z'
+				};
 
+		StringBuffer stringBuffer = new StringBuffer();
+		SecureRandom secureRandom = new SecureRandom();
+		secureRandom.setSeed(new Date().getTime());
+		
+		int index = 0;
+		int length = chars.length;
+		for (int i = 0; i < 8; i++) {
+			index = secureRandom.nextInt(length);
+		
+			if (index % 2 == 0) 
+				stringBuffer.append(String.valueOf(chars[index]).toUpperCase());
+			else
+				stringBuffer.append(String.valueOf(chars[index]).toLowerCase());
+		
+		}
+		
+		System.out.println("[AdminMemberService] NEW PASSWORD: " + stringBuffer.toString());
+		
+		return stringBuffer.toString();
+		
+	}
+	
+	private void sendNewPasswordByMail(String toMailAddr, String newPassword) {
+		System.out.println("[AdminMemberService] sendNewPasswordByMail()");
+		
+		final MimeMessagePreparator mimeMessagePreparator = new MimeMessagePreparator() {
+			
+			@Override
+			public void prepare(MimeMessage mimeMessage) throws Exception {
+				final MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+				mimeMessageHelper.setTo("cagim1@naver.com");
+//				mimeMessageHelper.setTo(toMailAddr);
+				mimeMessageHelper.setSubject("[한국 도서관] 새 비밀번호 안내입니다.");
+				mimeMessageHelper.setText("새 비밀번호 : " + newPassword, true);
+				
+			}
+			
+		};
+		javaMailSenderImpl.send(mimeMessagePreparator);
+		
+	}
+		
+	
 	
 }
