fdb3e4ae5e9a0c7f9d466a3fadeaeae4553d783d
cagim125 <cagim30@gmail.com>
2024-02-05 22:54:48 +0900

    유저 로그아웃 추가

--- BookRentalPjt/src/main/java/com/office/library/user/member/UserMemberController.java
+++ BookRentalPjt/src/main/java/com/office/library/user/member/UserMemberController.java
@@ -50,7 +50,7 @@ public class UserMemberController {
 		return nextPage;
 	}
 	
-	// 로그인 확인
+		// 로그인 확인
 		@PostMapping("/loginConfirm")
 		public String loginConfirm(UserMemberVo userMemberVo, HttpSession session) {
 			System.out.println("[UserMemberController] loginConfirm()");
@@ -66,6 +66,79 @@ public class UserMemberController {
 				session.setAttribute("loginedUserMemberVo", loginedUserMemberVo);
 				session.setMaxInactiveInterval(60 * 30);
 			}
+			
+			return nextPage;
+		}
+		
+		// 회원 정보 수정
+		@GetMapping("/modifyAccountForm")
+		public String modifyAccountForm(HttpSession session) {
+			System.out.println("[UserMemberController] modifyAccountForm()");
+			
+			String nextPage = "user/member/modify_account_form";
+			
+			UserMemberVo loginedUserMemberVo = 
+					(UserMemberVo) session.getAttribute("loginedUserMemberVo");
+			
+			if (loginedUserMemberVo == null)
+				nextPage = "user/member/login_form";
+			
+			return nextPage;
+		}
+		
+		// 회원 정보 수정 확인
+		@PostMapping("modifyAccountConfirm")
+		public String modifyAccountConfirm(UserMemberVo userMemberVo, HttpSession session) {
+			System.out.println("[UserMemberController] modifyAccountConfirm()");
+			
+			String nextPage = "user/member/modify_account_ok";
+			
+			int result = userMemberService.modifyAccountConfirm(userMemberVo);
+			
+			if(result > 0) {
+				UserMemberVo loginedUserMemberVo = 
+						userMemberService.getLoginedUserMemberVo(userMemberVo.getU_m_no());
+				
+				session.setAttribute("loginedUserMemberVo", loginedUserMemberVo);
+				session.setMaxInactiveInterval(60 * 30);
+			}else {
+				nextPage = "user/member/modify_account_ng";
+			}
+			
+			return nextPage;
+		}
+		
+		// 로그아웃
+		@GetMapping("/logoutConfirm")
+		public String logoutConfirm(HttpSession session) {
+			System.out.println("[UserMemberController] logoutConfirm()");
+			
+			String nextPage = "redirect:/";
+			
+			session.invalidate();
+			
+			return nextPage;
+		}
+		
+		@GetMapping("/findPasswordForm")
+		public String findPasswordForm(HttpSession session) {
+			System.out.println("[UserMemberController] findPasswordForm()");
+			
+			String nextPage = "user/member/find_password_form";
+			
+			return nextPage;
+		}
+		
+		@PostMapping("/findPasswordConfirm")
+		public String findPasswordConfirm(UserMemberVo userMemberVo) {
+			System.out.println("[UserMemberController] findPasswordConfirm()");
+			
+			String nextPage = "user/member/find_password_ok";
+			
+			int result = userMemberService.findPasswordConfirm(userMemberVo);
+			
+			if (result <= 0)
+				nextPage = "user/member/find_password_ng";
 			
 			return nextPage;
 		}
